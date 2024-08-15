833c08348aa2d405c9b818d806a67318b138d69c
LeviMaycon <levioficial361@gmail.com>
2024-08-14 21:53:37 -0300

    feat: add housemodule in appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { uploadModule } from './modules/Upload/upload.module';
+import { HouseModule } from './modules/House/house.module';
 
 @Module({
-  imports: [uploadModule],
+  imports: [uploadModule, HouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
